22ba7d327699f7a4be86c53dae17f544b34bb330
Huitzilin <huitzilin@Luiss-MacBook-Pro.local>
2022-02-21 13:07:01 -0600

    feat: set Navbar component
    Added basic elements to Navbar component

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
+// styles
+import style from "../../styles/Navbar.module.css";
+
 const Navbar: React.FC = () => {
-  return <div>index</div>;
+  return (
+    <nav className={style.navContainer}>
+      <span><Link href="/">Home</Link></span>
+      <ul>
+        <li><Link href="/findservices">Find services</Link></li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
