5aa8fc13bbeceb746eb01638101f4f5c90b4bc7a
CorregidorOscar <89716856+CorregidorOscar@users.noreply.github.com>
2022-02-21 17:05:52 -0300

    Update _app.tsx
    Añadido ChakraProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     //<SessionProvider session={pageProps.session}>
+    <ChakraProvider>
       <Component {...pageProps} />
+    </ChakraProvider>
     //</SessionProvider>
   );
 }
