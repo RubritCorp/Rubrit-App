3b168d92271bcc24a76e4a820b1900457ca92c98
Santiago Bancalari <bancalarisantiago@gmail.com>
2022-02-22 17:28:55 -0300

    style(find_services_component)

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -17,57 +17,56 @@ const testimonials = [
     name: "Brandon P.",
     role: "Chief Marketing Officer",
     content:
-      "It really saves me time and effort. It is exactly what our business has been lacking. EEZY is the most valuable business resource we have EVER purchased. After using EEZY my business skyrocketed!",
+      "Wilson was fantastic! Easy to work with, efficient, and went above and beyond when the task ended up being much larger/longer than we ant...",
     avatar:
       "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    categorie: "Plomeria",
   },
   {
     name: "Krysta B.",
     role: "Entrepreneur",
     content:
-      "I didn't even need training. We've used EEZY for the last five years. I have gotten at least 50 times the value from EEZY. I made back the purchase price in just 48 hours!",
+      "Essra was a pleasure to work with. Punctual, friendly, helpful and really knows how to pack a storage unit for maximum efficiency. Will h...",
     avatar:
       "https://images.unsplash.com/photo-1598550874175-4d0ef436c909?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    categorie: "Limpieza",
   },
   {
     name: "Darcy L.",
     role: "Movie star",
     content:
-      "Thank you for making it painless, pleasant and most of all, hassle free! I'm good to go. No matter where you go, EEZY is the coolest, most happening thing around! I love EEZY!",
+      "Hermann did a fantastic job installing a new AC unit in our house. His communication was excellent and he did really detailed work. I wil...",
     avatar:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80",
+    categorie: "Automotor",
   },
   {
     name: "Daniel T.",
     role: "Musician",
     content:
-      "I am so pleased with this product. EEZY is both attractive and highly adaptable. Without EEZY, we would have gone bankrupt by now. Thank you for creating this product!",
+      "Laura is AMAZING. She is, by far, the BEST we've found on TaskRabbit for cleaning jobs. She is extremely detailed and efficient. I defini...",
     avatar:
       "https://images.unsplash.com/photo-1606513542745-97629752a13b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    categorie: "Limpieza",
   },
   {
     name: "Daniel T.",
     role: "Musician",
     content:
-      "I am so pleased with this product. EEZY is both attractive and highly adaptable. Without EEZY, we would have gone bankrupt by now. Thank you for creating this product!",
+      "Dmitriy was great! Was fast to respond to the job, worked quickly but professionally, and had all the appropriate tools. Would recommend ",
     avatar:
       "https://images.unsplash.com/photo-1606513542745-97629752a13b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    categorie: "Profesor",
   },
   {
     name: "Daniel T.",
     role: "Musician",
     content:
-      "I am so pleased with this product. EEZY is both attractive and highly adaptable. Without EEZY, we would have gone bankrupt by now. Thank you for creating this product!",
+      "Nick did an outstanding job assembling my patio heater, and he got it done faster than I could have imagined. He also has a very pleasan...",
     avatar:
       "https://images.unsplash.com/photo-1606513542745-97629752a13b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    categorie: "Profesor",
   },
-];
-
-const backgrounds = [
-  `url("data:image/svg+xml, %3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'560\' height=\'185\' viewBox=\'0 0 560 185\' fill=\'none\'%3E%3Cellipse cx=\'102.633\' cy=\'61.0737\' rx=\'102.633\' ry=\'61.0737\' fill=\'%23ED64A6\' /%3E%3Cellipse cx=\'399.573\' cy=\'123.926\' rx=\'102.633\' ry=\'61.0737\' fill=\'%23F56565\' /%3E%3Cellipse cx=\'366.192\' cy=\'73.2292\' rx=\'193.808\' ry=\'73.2292\' fill=\'%2338B2AC\' /%3E%3Cellipse cx=\'222.705\' cy=\'110.585\' rx=\'193.808\' ry=\'73.2292\' fill=\'%23ED8936\' /%3E%3C/svg%3E")`,
-  `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='457.367' cy='123.926' rx='102.633' ry='61.0737' transform='rotate(-180 457.367 123.926)' fill='%23ED8936'/%3E%3Cellipse cx='160.427' cy='61.0737' rx='102.633' ry='61.0737' transform='rotate(-180 160.427 61.0737)' fill='%2348BB78'/%3E%3Cellipse cx='193.808' cy='111.771' rx='193.808' ry='73.2292' transform='rotate(-180 193.808 111.771)' fill='%230BC5EA'/%3E%3Cellipse cx='337.295' cy='74.415' rx='193.808' ry='73.2292' transform='rotate(-180 337.295 74.415)' fill='%23ED64A6'/%3E%3C/svg%3E")`,
-  `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='102.633' cy='61.0737' rx='102.633' ry='61.0737' fill='%23ED8936'/%3E%3Cellipse cx='399.573' cy='123.926' rx='102.633' ry='61.0737' fill='%2348BB78'/%3E%3Cellipse cx='366.192' cy='73.2292' rx='193.808' ry='73.2292' fill='%230BC5EA'/%3E%3Cellipse cx='222.705' cy='110.585' rx='193.808' ry='73.2292' fill='%23ED64A6'/%3E%3C/svg%3E")`,
-  `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='457.367' cy='123.926' rx='102.633' ry='61.0737' transform='rotate(-180 457.367 123.926)' fill='%23ECC94B'/%3E%3Cellipse cx='160.427' cy='61.0737' rx='102.633' ry='61.0737' transform='rotate(-180 160.427 61.0737)' fill='%239F7AEA'/%3E%3Cellipse cx='193.808' cy='111.771' rx='193.808' ry='73.2292' transform='rotate(-180 193.808 111.771)' fill='%234299E1'/%3E%3Cellipse cx='337.295' cy='74.415' rx='193.808' ry='73.2292' transform='rotate(-180 337.295 74.415)' fill='%2348BB78'/%3E%3C/svg%3E")`,
 ];
 
 interface TestimonialCardProps {
@@ -76,17 +75,19 @@ interface TestimonialCardProps {
   content: string;
   avatar: string;
   index: number;
+  categorie: string;
 }
 
 function TestmonialCard(props: TestimonialCardProps) {
-  const { name, role, content, avatar, index } = props;
+  const { name, role, content, avatar, categorie, index } = props;
 
   const theme = useTheme();
 
   return (
     <Flex
       boxShadow={"xl"}
-      maxW="100%"
+      maxW="90%"
+      margin={"0 auto"}
       direction={{ base: "column-reverse", md: "row" }}
       width={"full"}
       rounded={"xl"}
@@ -125,7 +126,7 @@ function TestmonialCard(props: TestimonialCardProps) {
               alignSelf={"center"}
             />
           </Box>
-          <Flex>
+          <Flex margin={"1em"}>
             <Star size={12} weight="fill" color={theme.colors.medium_green} />
             <Star size={12} weight="fill" color={theme.colors.medium_green} />
             <Star size={12} weight="fill" color={theme.colors.medium_green} />
@@ -134,7 +135,7 @@ function TestmonialCard(props: TestimonialCardProps) {
           </Flex>
         </Flex>
         <Flex flexDirection="column">
-          <chakra.p fontFamily={"Roboto"} fontWeight={"bold"} fontSize="1rem">
+          <chakra.p fontFamily={"Poppins"} fontWeight={"bold"} fontSize="1rem">
             {name}
             <chakra.span
               fontFamily={"Roboto"}
@@ -145,8 +146,16 @@ function TestmonialCard(props: TestimonialCardProps) {
               - {role}
             </chakra.span>
           </chakra.p>
-          <chakra.p fontFamily={"Poppins"} fontSize={"15px"} pb={4}>
+          <chakra.p fontFamily={"Poppins"} fontSize={"1rem"} pb={4}>
             {content}
+          </chakra.p>
+          <chakra.p
+            fontFamily={"Roboto"}
+            fontWeight={600}
+            pb={4}
+            color={theme.colors.medium_green}
+          >
+            {categorie}
           </chakra.p>
         </Flex>
       </Flex>
--- src/components/FindServices/index.tsx
+++ src/components/FindServices/index.tsx
@@ -10,6 +10,7 @@ import {
   Heading,
   Button,
   useTheme,
+  SimpleGrid,
 } from "@chakra-ui/react";
 import {
   CalendarCheck,
@@ -28,6 +29,7 @@ import Link from "next/link";
 import Layout from "../layout";
 import Comments from "../Comments";
 //styles
+import styles from "./FindServices.module.css";
 
 const FindServices: React.FC = () => {
   const theme = useTheme();
@@ -40,141 +42,157 @@ const FindServices: React.FC = () => {
             COMO FUNCIONA RUBRIT
           </Text>
         </Flex>
-        <Box>
-          <Flex margin={5}>
-            <Stack direction="row" padding={5}>
-              <NumberCircleOne
-                size={80}
-                weight="fill"
-                color={theme.colors.medium_green}
-              />
-            </Stack>
+        <Flex>
+          <Box>
+            <div className={styles.containerCircles}>
+              <div className={styles.circle}>
+                <NumberCircleOne
+                  size={80}
+                  weight="fill"
+                  color={theme.colors.medium_green}
+                />
+                <div className={styles.line}></div>
+              </div>
+              <div className={styles.circle}>
+                <NumberCircleTwo
+                  size={80}
+                  weight="fill"
+                  color={theme.colors.medium_green}
+                />
+                <div className={styles.line}></div>
+              </div>
+              <div className={styles.circle}>
+                <NumberCircleThree
+                  size={80}
+                  weight="fill"
+                  color={theme.colors.medium_green}
+                />
+                <div className={styles.line}></div>
+              </div>
+              <div className={styles.circle}>
+                <NumberCircleFour
+                  size={80}
+                  weight="fill"
+                  color={theme.colors.medium_green}
+                />
+              </div>
+            </div>
+          </Box>
 
-            <Box margin={5}>
-              <Flex alignItems="center" marginBottom={2}>
-                <CalendarCheck
-                  size={70}
-                  weight="regular"
-                  color={theme.colors.medium_green}
-                />
-                <Heading size="lg" marginLeft={5}>
-                  DESCRIBI LAS TAREAS
-                </Heading>
+          <Box>
+            <SimpleGrid columns={1}>
+              <Flex>
+                <Box margin={5} height="200px">
+                  <Flex alignItems="center" marginBottom={2}>
+                    <CalendarCheck
+                      size={70}
+                      weight="regular"
+                      color={theme.colors.medium_green}
+                    />
+                    <Heading size="lg" marginLeft={5}>
+                      DESCRIBI LAS TAREAS
+                    </Heading>
+                  </Flex>
+                  <Text fontSize="1.5rem">
+                    Haces una breve descripcion del trabajo que estas
+                    necesitando hacer. Si queres podes adjuntar fotos para que
+                    el profesional que lo vea tenga una idea mas exacta de lo
+                    que hay que hacer.
+                  </Text>
+                </Box>
               </Flex>
-              <Text fontSize="1.5rem">
-                Haces una breve descripcion del trabajo que estas necesitando
-                hacer. Si queres podes adjuntar fotos para que el profesional
-                que lo vea tenga una idea mas exacta de lo que hay que hacer.
-              </Text>
-            </Box>
-          </Flex>
-          <Flex margin={5}>
-            <Stack direction="row" padding={5}>
-              <NumberCircleTwo
-                size={80}
-                weight="fill"
-                color={theme.colors.medium_green}
-              />
-            </Stack>
-
-            <Box margin={5}>
-              <Flex alignItems="center" marginBottom={2}>
-                <MagnifyingGlass
-                  size={70}
-                  weight="regular"
-                  color={theme.colors.medium_green}
-                />
-                <Heading size="lg" marginLeft={5}>
-                  BUSCAR PROFESIONALES Y PRECIOS
-                </Heading>
+              <Flex>
+                <Box margin={5} height="200px">
+                  <Flex alignItems="center" marginBottom={2}>
+                    <MagnifyingGlass
+                      size={70}
+                      weight="regular"
+                      color={theme.colors.medium_green}
+                    />
+                    <Heading size="lg" marginLeft={5}>
+                      BUSCAR PROFESIONALES Y PRECIOS
+                    </Heading>
+                  </Flex>
+                  <Text fontSize="1.5rem">
+                    Busca en nuestra lista de profesionales cual te parece el
+                    mas indicado, vas a poder elegir varios filtros. Entre
+                    estos, calificaciones, reseñas, material grafico de sus
+                    trabajos.
+                  </Text>
+                </Box>
               </Flex>
-              <Text fontSize="1.5rem">
-                Busca en nuestra lista de profesionales cual te parece el mas
-                indicado, vas a poder elegir varios filtros. Entre estos,
-                calificaciones, reseñas, material grafico de sus trabajos.
-              </Text>
-            </Box>
-          </Flex>
-          <Flex margin={5}>
-            <Stack direction="row" padding={5}>
-              <NumberCircleThree
-                size={80}
-                weight="fill"
-                color={theme.colors.medium_green}
-              />
-            </Stack>
-            <Box margin={5}>
-              <Box display="flex" alignItems="center" marginBottom={2}>
-                <Calendar
-                  size={70}
-                  weight="regular"
-                  color={theme.colors.medium_green}
-                />
-                <Heading size="lg" marginLeft={5}>
-                  ELEGI FECHA Y HORA
-                </Heading>
-              </Box>
-              <Text fontSize="1.5rem">
-                Selecciona segun la disponibilidad horaria del profesional un
-                horario que te quede comodo para que se pueda realizar el
-                trabajo .
-              </Text>
-            </Box>
-          </Flex>
-          <Flex margin={5}>
-            <Stack direction="row" padding={5}>
-              <NumberCircleFour
-                size={80}
-                weight="fill"
-                color={theme.colors.medium_green}
-              />
-            </Stack>
-            <Box margin={5}>
-              <Box display="flex" alignItems="center" marginBottom={2}>
-                <CheckCircle
-                  size={70}
-                  weight="regular"
-                  color={theme.colors.medium_green}
-                />
-                <Heading size="lg" marginLeft={5}>
-                  CONFIRMA
-                </Heading>
-              </Box>
-              <Text fontSize="1.5rem">
-                Felicitaciones!!! ya completaste todo el formulario. nunca antes
-                fue tan facil y seguro contratar a un profesional para que te
-                haga el trabajo.
-              </Text>
-            </Box>
-          </Flex>
-        </Box>
+              <Flex>
+                <Box margin={5} height="200px">
+                  <Box display="flex" alignItems="center" marginBottom={2}>
+                    <Calendar
+                      size={70}
+                      weight="regular"
+                      color={theme.colors.medium_green}
+                    />
+                    <Heading size="lg" marginLeft={5}>
+                      ELEGI FECHA Y HORA
+                    </Heading>
+                  </Box>
+                  <Text fontSize="1.5rem">
+                    Selecciona segun la disponibilidad horaria del profesional
+                    un horario que te quede comodo para que se pueda realizar el
+                    trabajo .
+                  </Text>
+                </Box>
+              </Flex>
+              <Flex>
+                <Box margin={5}>
+                  <Box display="flex" alignItems="center" marginBottom={2}>
+                    <CheckCircle
+                      size={70}
+                      weight="regular"
+                      color={theme.colors.medium_green}
+                    />
+                    <Heading size="lg" marginLeft={5}>
+                      CONFIRMA
+                    </Heading>
+                  </Box>
+                  <Text fontSize="1.5rem">
+                    Felicitaciones!!! ya completaste todo el formulario. nunca
+                    antes fue tan facil y seguro contratar a un profesional para
+                    que te haga el trabajo.
+                  </Text>
+                </Box>
+              </Flex>
+            </SimpleGrid>
+          </Box>
+        </Flex>
         <Center height="20px">
           <Divider orientation="horizontal" />
         </Center>
-        <Flex direction="column">
+        <Flex direction="column" marginTop={10}>
           <Heading>ESTAS LISTO PARA BUSCAR QUIEN TE HAGA EL TRABAJO</Heading>
           <Text fontSize="1.5rem" textAlign="center">
             Chatea rápidamente con profesionales de servicios para tu casa.
             ¡Elige entre +175 categorías!
           </Text>
-          <Flex justifyContent="center" padding={10}>
+          <Flex justifyContent="center" padding={5}>
             <Flex flexDirection="column">
-              <Box
-                as="button"
-                width="20rem"
-                height="3rem"
-                borderRadius="10px"
-                bg={theme.colors.medium_green}
-                color="white"
-                fontSize="1.8rem"
-              >
-                COMENZAR
-              </Box>
+              <Link href="/">
+                <Box
+                  as="button"
+                  width="20rem"
+                  height="3rem"
+                  borderRadius="10px"
+                  bg={theme.colors.medium_green}
+                  color="white"
+                  fontSize="1.8rem"
+                >
+                  COMENZAR
+                </Box>
+              </Link>
               <Flex flexDirection="row" justifyContent="center" padding={2}>
                 <Text marginRight={4}>YA TENES UNA CUENTA?</Text>
-                <a href="/">
-                  <Text color={theme.colors.medium_green}>LOG IN</Text>
-                </a>
+                <Link href="/">
+                  <Text color={theme.colors.medium_green}>
+                    <span className={styles.link}>LOG IN</span>
+                  </Text>
+                </Link>
               </Flex>
             </Flex>
           </Flex>
