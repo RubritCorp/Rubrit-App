c8153fb2f7d0c28fa66d381907565174d9bace7c
unknown <elianasciclone@gmail.com>
2022-03-02 16:25:07 -0300

    component stepsone stepstwo requestsend

---
+++ src/pages/stepstwo.tsx
@@ -0,0 +1,15 @@
+import Layout from "components/layout";
+import Stepstwo from "../components/Stepstwo";
+
+const index = () => {
+    return (
+      <Layout
+        title="Todos los servicios en un solo lugar"
+        description="Encuentra servicios brindados por profesionales de confianza."
+      >
+        <Stepstwo />
+      </Layout>
+    );
+  };
+
+export default index
