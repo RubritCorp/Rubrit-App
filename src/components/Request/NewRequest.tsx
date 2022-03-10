9555c0ebd7324fd2f695ea59cf0e0459d050f407
huitzilin <techstemonline@gmail.com>
2022-03-10 07:26:14 -0600

    feat: new request conditional render

--- src/components/Request/NewRequest.tsx
+++ src/components/Request/NewRequest.tsx
@@ -1,12 +1,16 @@
 import { Container } from "@chakra-ui/react";
 import PublicRequest from "./PublicRequest";
 import PrivateRequest from "./PrivateRequest";
+import { useRouter } from "next/router";
 
 
 const NewRequest: React.FC = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
   return (
     <Container maxW={"container.xl"} py={12}>
-        { 1===1 ? <PublicRequest /> : <PrivateRequest />}
+        { !id ? <PublicRequest /> : <PrivateRequest />}
     </Container>
   );
 }
