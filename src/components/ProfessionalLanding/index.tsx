a5bdd8e026511e00936c017596e5cb7a158ea769
Huitzilin <99902364+huitzilin@users.noreply.github.com>
2022-03-11 10:15:31 -0600

    fix: remove 'as' from Link

--- src/components/ProfessionalLanding/index.tsx
+++ src/components/ProfessionalLanding/index.tsx
@@ -118,7 +118,7 @@ const ProfessionalLanding: React.FC = () => {
                 </Flex>
               </Flex>
               <Flex flexDirection={"column"} alignItems={"center"}>
-                <Link href={{ pathname: '/request/new', query: { id: '123' } }} as='nueva-solicitud' passHref>
+                <Link href={{ pathname: '/request/new', query: { id: '123' } }} passHref>
                   <Box
                     as={"button"}
                     width={"15rem"}
