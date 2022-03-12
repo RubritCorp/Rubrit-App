5d2539209925751849eb4fc65cffc84e52b695c7
Santiago Bancalari <bancalarisantiago@gmail.com>
2022-03-11 22:03:06 -0300

    Type UserLandin component

--- src/pages/professional/[userId].tsx
+++ src/pages/professional/[userId].tsx
@@ -2,7 +2,7 @@ import ProfessionalLanding from "../../components/ProfessionalLanding";
 
 import { useRouter } from "next/router";
 
-const UserLanding: any = () => {
+const UserLanding: React.FC = () => {
   const { query } = useRouter();
   return (
     <>
