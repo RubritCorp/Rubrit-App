f1482a3ef28493c6851e2022ad9ad30c3bc6cc0b
IlCabezon <aguscastro2014.ac@gmail.com>
2022-03-20 16:47:59 -0300

    feat(puliendoCositas)

--- src/pages/api/user/updatePassword/index.ts
+++ src/pages/api/user/updatePassword/index.ts
@@ -11,7 +11,7 @@ import {
   verifyPassword,
 } from "utils/verifyPassword";
 import { SMTPClient } from "emailjs";
-import envConfig from "../../../../next-env-config";
+import envConfig from "../../../../../next-env-config";
 
 interface ICases {
   PUT(req: NextApiRequest, res: NextApiResponse<DataUpdate>): void;
@@ -49,6 +49,14 @@ const cases: ICases = {
 
       if (code) {
         const isValid = await verifyCode(code, user.authCode);
+
+        const samePassword = await verifyPassword(newPassword, user.password);
+
+        if (samePassword) {
+          return res.status(200).json({
+            message: "La nueva contraseña no puede ser igual a la anterior!",
+          });
+        }
 
         if (!isValid) {
           return res
@@ -106,13 +114,36 @@ const cases: ICases = {
           subject: "Cambio de contraseña",
           attachment: [
             {
-              data: `<h1>Cambio de la contraseña</h1><h2>Hola de nuevo</h2>         
-            <p>¡Hemos detectado que solicito un cambio de contraseña. Para realizar el cambio ingresa al siguiente enlace!</p>         
+              data: `
+              <div style="width: 100%;height: 40rem; background-color: #e0e0e0;display: flex;justify-content: center;align-items: center; padding: 0; margin: 0;">
+    <div style="max-width: 40rem; height: 90%;background: #fafafa;border-radius: 10px;margin:auto;">
+      <div style="display: flex; justify-content: center;">
+        <img src="https://rubrit-development.s3.sa-east-1.amazonaws.com/assets/logo-rubritt" alt="'logo" width="235px" height="75px" style="padding: 2rem;margin:auto;"/>
+      </div>
+      <div style="padding: 2rem;">
+        <h1 style="text-align: center;">¡Reforcemos la Seguridad de tu Cuenta!</h1>
+        <h3 style="text-align: center;">¡Hola ${
+          user.name
+        }, es un placer comunicarnos contigo nuevamente!.</h3>
+        <div style="height: 1rem; width: 100%;display: flex;justify-content: center;margin:auto;">
+          <div style="height: 0.5rem;width: 90%;background-color: #2EB67D;border-radius: 10px;margin:auto;">
+
+          </div>
+        </div>
+        <p style="text-align:center">¡Hemos detectado que solicitaste un cambio de contraseña, nos alegra que hayas decidido actualizar tu contraseña. Para realizar el cambio ingresa al siguiente enlace!</p>
+        <div style="width: 100%;display: flex;justify-content: center;height: 3rem;align-items: center;" >
+          <div style="width: max-content;height: 2.5rem;border-radius: 8px;background-color: #2EB67D;display: flex;justify-content: center;align-items: center;margin:auto;">
             <a href=${
               isAuthenticated
                 ? envConfig?.forgotPasswordAuthenticated
                 : envConfig?.redirectForgotPassword
-            }code=${code}&email=${email}> Click aquí</a></div>`,
+            }code=${code}&email=${email} style="color: #fafafa;text-decoration: none;margin:auto;padding-left:1rem;padding-right:1rem;">¡Cambia tu Contraseña!</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>        
+            `,
               alternative: true,
             },
           ],
