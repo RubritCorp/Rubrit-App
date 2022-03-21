10acc2479707c48fd931d0b95c307622193b5cc0
unknown <elianasciclone@gmail.com>
2022-03-21 14:57:12 -0300

    images_prof_landing

--- src/components/ProfessionalLanding/index.tsx
+++ src/components/ProfessionalLanding/index.tsx
@@ -15,6 +15,7 @@ import {
   Grid,
   GridItem,
 } from "@chakra-ui/react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 import { Star, Check, Checks, CheckCircle } from "phosphor-react";
 
 //native libraries
@@ -33,10 +34,13 @@ const ProfessionalLanding: React.FC<any> = (props) => {
   const [loading, setLoading] = useState<boolean>(false);
   const user = JSON.parse(props.user);
   const { workerData } = user;
+  //average rating
   const scoreTotal = Math.ceil(
     user.rating?.reduce((total: any, el: any) => (total += el.score), 0) /
       user.rating.length
   );
+  //pagination states
+  const [numberPage, setNumberPage] = useState<number>(0);
 
   if (!user) return <Loading />;
   return (
@@ -165,7 +169,7 @@ const ProfessionalLanding: React.FC<any> = (props) => {
             </Flex>
           </Flex>
         </Flex>
-            <Container maxW={"container.xl"}>
+        <Container maxW={"container.xl"}>
           <Flex flexDirection={"column"}>
             <Heading
               fontSize={{
@@ -180,8 +184,8 @@ const ProfessionalLanding: React.FC<any> = (props) => {
             <Text fontSize={{ base: "0.9rem", md: "1.2rem", lg: "1.4rem" }}>
               {user.description}
             </Text>
-         
-          <Divider margin={"1em 0"}></Divider>
+
+            <Divider margin={"1em 0"}></Divider>
             <Flex
               w={"100%"}
               flexWrap={{
@@ -189,20 +193,107 @@ const ProfessionalLanding: React.FC<any> = (props) => {
                 md: "wrap",
                 lg: "nowrap",
               }}
+              justifyContent={"space-between"}
             >
-              <Stack><Flex flexDirection={"column"}>
-                <Box margin={"1em 0"}>
-                  <Heading
-                    fontSize={{
-                      base: "1rem",
-                      md: "1.2rem",
-                      lg: "1.5rem",
-                    }}
-                    color={"light_grey_sub"}
-                  >
-                    TRABAJOS REALIZADOS
-                  </Heading>
-                  <Flex
+              <Stack>
+                <Flex flexDirection={"column"} justifyContent={"space-between"}>
+                  <Box>
+                    <Heading
+                      fontSize={{
+                        base: "1rem",
+                        md: "1.2rem",
+                        lg: "1.5rem",
+                      }}
+                      color={"light_grey_sub"}
+                    >
+                      TRABAJOS REALIZADOS
+                    </Heading>
+                    
+                    <Container>
+                    <Flex
+                      flexWrap={"wrap"}
+                      justifyContent={"space-between"}
+                      w={"500px"}
+                    >
+                      {workerData.images?.slice(numberPage, numberPage + 4)
+                        .map((m: any, i: number) => (
+                          <Box
+                            key={i}
+                            position={"relative"}
+                            w={ "48%" }
+                            h={"150px"}
+                            bgGradient="linear(to-r, #ddd, #e8e8e8)"
+                            mt={7}
+                            backgroundImage={m}
+                            backgroundPosition={"center"}
+                            backgroundSize={"cover"}
+                            borderRadius={7}
+                          ></Box>
+                        ))}
+                    </Flex></Container>
+
+                    <Flex
+                      justifyContent={{ base: "center", md: "space-evenly" }}
+                      flexDirection={{ base: "column", md: "row" }}
+                      mt={3}
+                    >
+                      <Text
+                        color="gray"
+                        fontSize={{ base: "sm", md: "lg" }}
+                        textAlign={{ base: "start", md: "start" }}
+                      >
+                        Mostrando{" "}
+                        {
+                          workerData.images.slice(numberPage, numberPage + 4)
+                            .length
+                        }{" "}
+                        de {workerData.images.length}{" "}
+                        {workerData.images.length === 1
+                          ? "Elemento"
+                          : "Elementos"}
+                      </Text>
+
+                      <Flex w={{ base: "100%", md: "50%" }}>
+                        {numberPage !== 0 ? (
+                          <Text
+                            cursor={"pointer"}
+                            onClick={() => {
+                              numberPage === 0
+                                ? ""
+                                : setNumberPage(numberPage - 4);
+                            }}
+                            width={"50%"}
+                            textAlign={"center"}
+                          >
+                            <ChevronLeftIcon w={6} h={6} />
+                            Anterior
+                          </Text>
+                        ) : (
+                          <Text width={"50%"}></Text>
+                        )}
+                        {numberPage / 4 + 1 !==
+                          Math.ceil(workerData.images.length / 4) &&
+                        workerData.images.length !== 0 ? (
+                          <Text
+                            cursor={"pointer"}
+                            onClick={() => {
+                              numberPage / 4 ===
+                              Math.ceil(workerData.images.length / 4) - 1
+                                ? ""
+                                : setNumberPage(numberPage + 4);
+                            }}
+                            width={"50%"}
+                            textAlign={"right"}
+                          >
+                            Siguiente
+                            <ChevronRightIcon w={6} h={6} />
+                          </Text>
+                        ) : (
+                          <Text width={"50%"}></Text>
+                        )}
+                      </Flex>
+                    </Flex>
+                    {/* <Flex
                     flexDirection={"row"}
                     flexWrap={{ base: "wrap", md: "wrap", lg: "nowrap" }}
                   >
@@ -221,83 +312,98 @@ const ProfessionalLanding: React.FC<any> = (props) => {
                         );
                       }
                     })}
-                  </Flex>
-                  <Divider margin={"1em 0"}></Divider>
-                </Box>
+                  </Flex> */}
+                    <Divider margin={"1em 0"}></Divider>
+                  </Box>
+{/* DOCUMENTACION */}
                   <Heading
-                    fontSize={{
-                      base: "1rem",
-                      md: "1.2rem",
-                      lg: "1.5rem",
-                    }}
-                    color={"light_grey_sub"}
-                  >
-                    DOCUMENTACION
-                  </Heading>
-                  <Flex
-                    flexDirection={"row"} justifyContent={"left"}
-                    flexWrap={{ base: "wrap", md: "wrap", lg: "nowrap" }}
-                  >
-                    {user.workerData.certification.map((n: any, i: number) => {
-                      if (i < 3) {
-                        return (
-                          <Image
-                            p={1.5}
+                      fontSize={{
+                        base: "1rem",
+                        md: "1.2rem",
+                        lg: "1.5rem",
+                      }}
+                      color={"light_grey_sub"}
+                    >
+                      DOCUMENTACION
+                    </Heading>
+                    <Flex
+                      flexWrap={"wrap"}
+                      justifyContent={"space-between"}
+                      w={"500px"}
+                    >
+                      {workerData.certification
+                        .map((m: any, i: number) => {
+                          if (i < 2) {
+                            return (
+                          <Box
                             key={i}
-                            src={n}
-                            alt="worker-cert"
-                            maxW="12em"
-                            borderRadius={"0.3rem"}
-                            marginTop={"1rem"}
-                          />
-                        );
-                      }
-                    })}
-                  </Flex>
+                            position={"relative"}
+                            w={"48%"}
+                            h={"150px"}
+                            bgGradient="linear(to-r, #ddd, #e8e8e8)"
+                            mt={7}
+                            backgroundImage={m}
+                            backgroundPosition={"center"}
+                            backgroundSize={"cover"}  
+                          >
+                          </Box>)}
+                        })}
+                    </Flex>
+
+
                   <Divider margin={"1em 0"}></Divider>
                 </Flex>
-              </Stack><Flex flexDirection={"column"} margin={{ base: "0", md: "0", lg: "1rem" }} justifyContent="center">
-              <Box>
-                <Heading
-                  fontSize={{ base: "1rem", md: "1.2rem", lg: "1.5rem" }}
-                  color={"light_grey_sub"}
-                  marginLeft="0.6em"
-                >
-                  UBICACION
-                </Heading>
-                <Box
-                  height={{ base: "23em", md: "25em", lg: "27em" }}
-                  width={{ base: "24em", md: "28em", lg: "34em" }}
-                  margin={"1.3em 0 1.7em 1em"}
-                >
-                  {user.address.lat && user.address.lng ? (
-                    <Map
-                      location={{
-                        lat: user.address.lat,
-                        lng: user.address.lng,
-                      }}
-                      coverage={user.address.searchRange}
-                    />
-                  ) : (
-                    <Map />
-                  )}
+              </Stack>
+              <Flex
+                flexDirection={"column"}
+                margin={{ base: "0", md: "0", lg: "1rem" }}
+                
+              >
+                <Box>
+                  <Heading
+                    fontSize={{ base: "1rem", md: "1.2rem", lg: "1.5rem" }}
+                    color={"light_grey_sub"}
+                    marginLeft="0.6em"
+                  >
+                    UBICACION
+                  </Heading>
+                  <Box
+                    height={{ base: "23em", md: "25em", lg: "27em" }}
+                    width={{ base: "26em", md: "36em", lg: "40em" }}
+                    margin={"1.3em 0 1.7em 1em"}
+                  >
+                    {user.address.lat && user.address.lng ? (
+                      <Map
+                        location={{
+                          lat: user.address.lat,
+                          lng: user.address.lng,
+                        }}
+                        coverage={user.address.searchRange}
+                      />
+                    ) : (
+                      <Map />
+                    )}
+                  </Box>
                 </Box>
-              </Box></Flex>
+              </Flex>
             </Flex>
             <Container maxW={"container.lg"}>
-              <Flex flexDirection={"row"}
-          justifyContent={"space-evenly"}         
-          flexWrap={{ base: "wrap", md: "wrap", lg: "wrap" }} marginBottom={10}>
-                <Flex
-                  maxH={"540px"}
-                  overflowY="auto"
-                >
+              <Flex
+                flexDirection={"row"}
+                justifyContent={"space-evenly"}
+                flexWrap={{ base: "wrap", md: "wrap", lg: "wrap" }}
+                marginBottom={10}
+              >
+                <Flex maxH={"540px"} overflowY="auto">
                   <Comments {...{ user }} />
                 </Flex>
-                <Box borderRadius={"10px"} margin={{ base: "2em", lg: "2em 0 0 10em"}}>
+                <Box
+                  borderRadius={"10px"}
+                  margin={{ base: "2em", lg: "2em 0 0 10em" }}
+                >
                   <Flex
                     flexDirection={"column"}
-                    padding={{ base: "1em", lg: "2em"}}
+                    padding={{ base: "1em", lg: "2em" }}
                     boxShadow={"lg"}
                     overflowY={"auto"}
                   >
