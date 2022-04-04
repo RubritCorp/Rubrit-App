7b6de8fede594b5ba25ee7d893b1fbe15965e9f7
Santiago Bancalari <bancalarisantiago@gmail.com>
2022-04-04 13:32:47 -0300

    test

--- src/pages/comunityPrinciply.tsx
+++ src/pages/comunityPrinciply.tsx
@@ -1,3 +1,3 @@
-import ComunityPrinciply from '../components/ComunityPrinciply/comunityPrinciply';
+import ComunityPrinciply from "../components/ComunityPrinciply/comunityPrinciply";
 
 export default ComunityPrinciply;
